86a07d56d47821216adb8813e6ba86423e2b9db9
Nicolai Wolfrom <nicolai-wolfrom@gmx.de>
2020-01-03 17:14:03 +0100

    Build first website structure using "rails generate controller pages index about contact" and redirected them to the routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # set index
   root to: "pages#index"
+
+  # redirect domain/contact
+  get 'contact', to: 'pages#contact'
+
+  # redirect domain/about
+  get 'about', to: 'pages#about'
+
 end
